fcd6d5ee26ff512cda1cbc1fdef8ca4d62c60678
sudharam sur <67544790+sudharamsur@users.noreply.github.com>
2021-08-14 02:12:15 +0600

    Add files via upload

---
+++ URI/Salary.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    int A,B;
+    float C,X;
+    scanf ("%d %d",&A,&B);
+    scanf ("%f",&C);
+    X = B*C;
+    printf ("NUMBER = %d\n",A);
+    printf ("SALARY = u$ %.2f",X);
+    return 0;
+}
